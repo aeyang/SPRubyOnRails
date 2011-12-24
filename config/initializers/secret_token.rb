f0c09ac2a6c167adb8890df3657c45c7ca6c4b9c
Alan Yang <alanyang@sbcglobal.net>
2011-12-24 01:08:13 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LastFM::Application.config.secret_token = 'd9718f2a7499fc7c970db4c6a020423779bc9f9d12975ba3083907fe5492422e5668697fc14158efd710cddf2d025b0fc2ed989e429c1c7d3a9a19aeab181b74'
