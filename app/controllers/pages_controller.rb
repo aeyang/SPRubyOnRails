327b24854e9b763f4769063ebcc10aea329ba09e
Alan Yang <alanyang@sbcglobal.net>
2012-01-07 14:03:29 -0800

    Failing Amazon gem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,7 +12,25 @@ class PagesController < ApplicationController
   end
 
   def Amazon
-  	@title = "Amazon"
+    @title = "Amazon"
+  	#Amazon_product gem setup
+  	req = AmazonProduct['us']
+  	req.configure do |c|
+  	  c.key = 'AKIAJKWWS7XBCREYXFBA'
+  	  c.secret = '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'
+  	  c.tag = 'musiexplsenip-20'
+    end
+  	
+  	req << {
+  		operation => 'ItemSearch',
+  		:search_index => 'All',
+  		:Keywords => 'Book',
+  		:response_group => ['ItemAttributes', 'Images'],
+  		:Version => '2011-08-01'
+  	}
+
+  	@resp = req.get
+  	#@resp = req.search('George Orwell')
   end
 
 end
