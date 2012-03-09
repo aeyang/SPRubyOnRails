41fe0847479cc3bb34b5b1d5aacdebd034a23d0a
Alan Yang <alanyang@sbcglobal.net>
2012-03-08 21:06:17 -0800

    fixed html tags in search and made slight hover effect for amazon

--- app/assets/javascripts/pages/amazon.js
+++ app/assets/javascripts/pages/amazon.js
@@ -1,29 +1,17 @@
-/*var amzn_wdgt;
-
-$(function() {
+$(function(){
 	if($("#amazon").length != 0){
-		addAmazonWidget();
+		
+		$("#amazon_content_div div").hover(show, noshow);
 	}
 });
 
-function addAmazonWidget(){
-	$.getScript('http://wms.assoc-amazon.com/20070822/US/js/AmazonWidgets.js', function() {
-		amzn_wdgt={widget:'Search'};
-		amzn_wdgt.tag='musiexplsenip-20';
-		amzn_wdgt.columns='5';
-		amzn_wdgt.rows='5';
-		amzn_wdgt.defaultSearchTerm='Music';
-		amzn_wdgt.searchIndex='Music';
-		amzn_wdgt.width='256';
-		amzn_wdgt.showImage='True';
-		amzn_wdgt.showPrice='True';
-		amzn_wdgt.showRating='True';
-		amzn_wdgt.design='2';
-		amzn_wdgt.colorTheme='Default';
-		amzn_wdgt.headerTextColor='#FFFFFF';
-		amzn_wdgt.outerBackgroundColor='#000000';
-		amzn_wdgt.marketPlace='US';
+function show(){
+	console.log($(this));
+  //$(this).hide();
+  $(this).children('span').show();
+}
 
-		$("#amazon_widget_div").appendChild(amzn_wdgt);
-	});
-}*/
+function noshow(){
+	$(this).children('span').hide('fast');
+  //$(this).show('fast');
+}
